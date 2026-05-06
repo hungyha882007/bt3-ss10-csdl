d28f6ac5f92499d7c5bbbba10d6c96e8daffbd49
hungyha882007 <hungyha882007@gmail.com>
2026-05-06 09:38:30 +0700

    Add files via upload

--- bt3ss10.sql
+++ bt3ss10.sql
@@ -1,35 +1,26 @@
-create database ss10bt3;
-use ss10bt3;
-
-
+create database test12;
+use test12;
 CREATE TABLE Departments (
     Dept_ID INT PRIMARY KEY,
     Dept_Name VARCHAR(100)
 );
-
-
-CREATE TABLE Patients (
-    Patient_ID INT PRIMARY KEY,
-    Full_Name VARCHAR(100)
-);
-
-
 CREATE TABLE Invoices (
     Invoice_ID INT PRIMARY KEY,
     Patient_ID INT,
     Dept_ID INT,
-    Amount DECIMAL(10,2),
-    FOREIGN KEY (Patient_ID) REFERENCES Patients(Patient_ID),
-    FOREIGN KEY (Dept_ID) REFERENCES Departments(Dept_ID)
+    Amount DECIMAL(10 , 2 )
 );
-
+INSERT INTO Departments VALUES (1, 'Nội' ), (2, 'Ngoại' );
+INSERT INTO Invoices VALUES (101, 1, 1, 500.00), (102, 2, 1, 300.00), (103, 3, 2,
+1000.00);
 CREATE VIEW Department_Revenue_View AS
 SELECT 
     d.Dept_Name,
     COUNT(DISTINCT i.Patient_ID) AS Total_Patients,
     SUM(i.Amount) AS Total_Revenue
 FROM Departments d
-LEFT JOIN Invoices i ON d.Dept_ID = i.Dept_ID
+JOIN Invoices i ON d.Dept_ID = i.Dept_ID
 GROUP BY d.Dept_Name;
-
-SELECT * FROM Department_Revenue_View;
+UPDATE Department_Revenue_View
+SET Total_Revenue = 9999
+WHERE Dept_Name = 'Nội';
